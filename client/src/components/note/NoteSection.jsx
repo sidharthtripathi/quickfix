f0aa8755d8c04a0b428aac47a06b00ddf62dbf10
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-31 02:45:57 +0530

    update note section

--- client/src/components/note/NoteSection.jsx
+++ client/src/components/note/NoteSection.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const NoteSection = () => {
-  return (
-    <div>NoteSection</div>
-  )
-}
+  const [inputList, setInputList] = useState([""]); // Initialize with one empty string for one input field
+  const inputRef = useRef(null);
 
-export default NoteSection
+  const onInputChange = (event, index) => {
+    // Update the value of the specific input field
+    const newInputList = [...inputList];
+    newInputList[index] = event.target.value;
+    setInputList(newInputList);
+  };
+
+  const onKeyUpHandler = (event, index) => {
+    if (event.keyCode === 13) {
+      // When Enter is pressed, add a new input field
+      console.log("length", inputList.length);
+      console.log('index',index);
+    if ( index === inputList.length -1 ){
+      setInputList([...inputList, ""]);
+    }else{
+        console.log(`Hello`)
+    }
+  };
+}
+  
+
+  return (
+    <div style={{ background: "white" }}>
+      {inputList.map((input, index) => (
+        <input
+          ref={index === inputList.length - 1 ? inputRef : null}
+          key={index}
+          style={{ width: "60vw" }}
+          value={input}
+          onChange={(event) => onInputChange(event, index)}
+          onKeyUp={(event) => onKeyUpHandler(event, index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NoteSection;
