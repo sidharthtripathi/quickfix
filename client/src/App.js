30eb3febccd69519deaf2a391f4a81afdd5153ef
sharmaishaan04 <ishaan401004@gmail.com>
2024-01-31 19:50:44 +0530

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import Login from "./components/auth/LoginPage";
 
 const App = () => {
-  return <RouterProvider router={router} />
+  // return <RouterProvider router={router} />
+  return <Login />;
 };
 
 export default App;
