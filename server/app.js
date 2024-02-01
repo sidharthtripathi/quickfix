b29809b9342aeb79c0f3ed750b702561c1a2003d
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-02-01 13:12:45 +0530

    fix some minor issue and refactored server code

--- server/app.js
+++ server/app.js
@@ -1,33 +1,29 @@
 const express = require("express");
-const app = express();
-
-const bodyParser=require("body-parser");
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors=require("cors");
-require("dotenv").config();
 
-const authRoute = require("./routes/auth");
+
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use("/",authRoute);
+const authRoute = require("./routes/auth");
 
-app.use((error,req,res)=>{
+app.use("/", authRoute);
 
-  
-  console.log("_______hello");
-  res.status(500).json({ success: false, message: error.message });
-})
+app.use((error, req, res, next) => {
+  res.status(500).json({ success: false, message: error.message || "something went wrong"});
+});
 
 const PORT = process.env.PORT || 1200;
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server started at port no: ${PORT}`));
-
   })
   .catch((error) => console.log(`${error} did not connect`));
