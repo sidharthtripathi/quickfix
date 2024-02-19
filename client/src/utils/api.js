e6d0f79d46bc71321a049800eeaa2e87e3ab5b1b
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-02-19 13:18:45 +0530

    add deleteData method in utils file

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 export const API = "http://localhost:1200";
+
+export const deleteData = (endpoint) => {
+  try {
+    const response = axios.delete(API + endpoint);
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+};
